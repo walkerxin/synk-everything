e092683f6334f496c5dceb82d36a130c786fd638
walkerxin <walkerxinH@gmail.com>
2022-04-19 16:00:02 +0800

    14」添加监听中断 v2：改用lorca窗口界面 效果：中断时关闭UI界面，关闭UI界面时关闭主进程
    Linux中启动时，仍有两个进程：go 和 synk-with-go.git

--- main.go
+++ main.go
@@ -1,14 +1,12 @@
 package main
 
 import (
-	"log"
 	"os"
-	"os/exec"
 	"os/signal"
-	"runtime"
 	"syscall"
 
 	"github.com/gin-gonic/gin"
+	"github.com/zserge/lorca"
 )
 
 // [需求]关闭界面时结束主进程，杀死主进程时关闭界面
@@ -22,30 +20,13 @@ func main() {
 		router.Run(":8080")
 	}()
 
-	var chromePath string
-	switch runtime.GOOS {
-	case "windows":
-		chromePath = os.Getenv("ProgramFiles(x86)") + "/Google/Chrome/Application/chrome.exe"
-	default:
-		chromePath = "/usr/bin/google-chrome-stable"
-	}
-	cmd := exec.Command(chromePath, "--app=http://localhost:8080")
-	cmd.Start()
+	ui, _ := lorca.New("http://localhost:8080", "", 600, 500)
 
 	chSignal := make(chan os.Signal, 1)
 	signal.Notify(chSignal, os.Interrupt, syscall.SIGINT, syscall.SIGTERM)
-	<-chSignal
-	err := cmd.Process.Kill()
-	if err != nil {
-		log.Println(cmd.Process.Pid, err)
+	select {
+	case <-chSignal:
+	case <-ui.Done():
 	}
-
-	// ui, _ := lorca.New("https://bilibili.com", "", 600, 400)
-	// chSignal := make(chan os.Signal, 1)
-	// signal.Notify(chSignal, syscall.SIGINT, syscall.SIGTERM)
-	// select {
-	// case <-chSignal:
-	// case <-ui.Done():
-	// }
-	// ui.Close()
+	ui.Close()
 }
