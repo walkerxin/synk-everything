b832ce491fa214831e03e50d5a5329e660b69262
walkerxin <walkerxinH@gmail.com>
2022-04-19 11:26:47 +0800

    12」13」引入 Gin
    细节：
    1. router.Run(":8080") 中 ListenAndServer 在死循环 for {}
    2. 书写方面：启动并监听某端口 ":8080"
    3. exec.Command 开启的是一个进程，启动一个go协程怎么都是比启动一个进程快的
    （gin服务器 与 UI界面 要注意顺序问题吗）

--- main.go
+++ main.go
@@ -2,13 +2,34 @@ package main
 
 import (
 	"os/exec"
+
+	"github.com/gin-gonic/gin"
 )
 
 // [需求]关闭界面时结束主进程，杀死主进程时关闭界面
 func main() {
+	go func() {
+		gin.SetMode(gin.DebugMode)
+		router := gin.Default()
+		router.GET("/", func(c *gin.Context) {
+			c.Writer.Write([]byte("abcdefg"))
+		})
+		router.Run(":8080")
+	}()
+
 	chromePath := "/usr/bin/google-chrome-stable"
-	cmd := exec.Command(chromePath, "--app=https://baidu.com")
+	cmd := exec.Command(chromePath, "--app=http://localhost:8080")
 	cmd.Start()
+	select {}
+	// 主线程结束，其中的协程也结束
+	/*
+		for {}
+		go程 无限 1k～1w个
+		ErrServerClose
+		
+		阻塞 就像等待戈多（同步读） 什么都不能做 挂起(快照，进程消失？)
+		死循环并不是阻塞
+	*/
 
 	// ui, _ := lorca.New("https://bilibili.com", "", 600, 400)
 	// chSignal := make(chan os.Signal, 1)
