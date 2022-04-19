0f743dd7787f4a55aee399d06fbfd638e3779856
walkerxin <walkerxinH@gmail.com>
2022-04-19 17:10:26 +0800

    「15」添加静态文件路由
    1. embed 使静态文件打包进可执行文件
    2. 静态文件批量映射
    3. 未匹配路由处理 router.NoRoute

--- main.go
+++ main.go
@@ -1,26 +1,51 @@
 package main
 
 import (
+	"embed"
+	"io/fs"
+	"log"
+	"net/http"
 	"os"
 	"os/signal"
+	"strings"
 	"syscall"
 
 	"github.com/gin-gonic/gin"
 	"github.com/zserge/lorca"
 )
 
-// [需求]关闭界面时结束主进程，杀死主进程时关闭界面
+//go:embed frontend/dist/*
+var FS embed.FS
+
 func main() {
 	go func() {
 		gin.SetMode(gin.DebugMode)
 		router := gin.Default()
-		router.GET("/", func(c *gin.Context) {
-			c.Writer.Write([]byte("abcdefg"))
+		staticFiles, _ := fs.Sub(FS, "frontend/dist")
+		// 静态路由
+		router.StaticFS("/static", http.FS(staticFiles))
+		router.NoRoute(func(c *gin.Context) {
+			path := c.Request.URL.Path
+			if strings.HasPrefix(path, "/static") {
+				reader, err := staticFiles.Open("index.html") // 处理错误
+				if err != nil {
+					log.Fatal(err)
+				}
+				defer reader.Close() // 在当前函数退出前执行
+				stat, err := reader.Stat()
+				if err != nil {
+					log.Fatal(err)
+				}
+				c.DataFromReader(http.StatusOK, stat.Size(), "text/html;", reader, nil)
+
+			} else {
+				c.Status(http.StatusNotFound)
+			}
 		})
 		router.Run(":8080")
 	}()
 
-	ui, _ := lorca.New("http://localhost:8080", "", 600, 500)
+	ui, _ := lorca.New("http://localhost:8080/static", "", 600, 500)
 
 	chSignal := make(chan os.Signal, 1)
 	signal.Notify(chSignal, os.Interrupt, syscall.SIGINT, syscall.SIGTERM)
