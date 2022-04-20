772c4c78ecb9dba2c95ca549a65a3fd66c5b80e0
walkerxin <walkerxinH@gmail.com>
2022-04-20 16:49:00 +0800

    17」实现文本上传
    用到了uuid库，随机生成文件名
    遇到一个疑惑：创建目录os.MkdirAll 或写入文件 ioutil.WriteFile 时,
    设置权限为 0777或os.ModePerm，应该是rwxrwxrwx，但最后是rwxrwxr-x
    0666 但 rw-rw-r--

--- main.go
+++ main.go
@@ -3,14 +3,18 @@ package main
 import (
 	"embed"
 	"io/fs"
+	"io/ioutil"
 	"log"
 	"net/http"
 	"os"
 	"os/signal"
+	"path"
+	"path/filepath"
 	"strings"
 	"syscall"
 
 	"github.com/gin-gonic/gin"
+	"github.com/google/uuid"
 	"github.com/zserge/lorca"
 )
 
@@ -22,12 +26,13 @@ func main() {
 		gin.SetMode(gin.DebugMode)
 		router := gin.Default()
 		staticFiles, _ := fs.Sub(FS, "frontend/dist")
+		router.POST("/api/v1/texts", TextsController)
 		// 静态路由
 		router.StaticFS("/static", http.FS(staticFiles))
 		router.NoRoute(func(c *gin.Context) {
 			path := c.Request.URL.Path
 			if strings.HasPrefix(path, "/static") {
-				reader, err := staticFiles.Open("index.html") // 处理错误
+				reader, err := staticFiles.Open("index.html") // 打开index文件，得到reader
 				if err != nil {
 					log.Fatal(err)
 				}
@@ -37,7 +42,6 @@ func main() {
 					log.Fatal(err)
 				}
 				c.DataFromReader(http.StatusOK, stat.Size(), "text/html;", reader, nil)
-
 			} else {
 				c.Status(http.StatusNotFound)
 			}
@@ -45,7 +49,7 @@ func main() {
 		router.Run(":8080")
 	}()
 
-	ui, _ := lorca.New("http://localhost:8080/static", "", 600, 500)
+	ui, _ := lorca.New("http://localhost:8080/static", "", 800, 600)
 
 	chSignal := make(chan os.Signal, 1)
 	signal.Notify(chSignal, os.Interrupt, syscall.SIGINT, syscall.SIGTERM)
@@ -54,4 +58,33 @@ func main() {
 	case <-ui.Done():
 	}
 	ui.Close()
+}
+
+func TextsController(c *gin.Context) {
+	var json struct { //1. 从body中获取用户上传的文本
+		Raw string
+	}
+	if err := c.ShouldBindJSON(&json); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+	} else {
+		exe, err := os.Executable() //2. 获取可执行文件所在的目录
+		if err != nil {
+			log.Fatal(err)
+		}
+		exeDir := filepath.Dir(exe)
+		uploadsDir := filepath.Join(exeDir, "uploads") //3. 上一步的目录拼接上 uploads，创建目录
+		err = os.MkdirAll(uploadsDir, os.ModePerm)
+		if err != nil {
+			log.Fatal(err)
+		}
+		log.Println("dir=" + uploadsDir)
+
+		filename := uuid.New().String() //4. 生成文件名
+		fullpath := path.Join("uploads", filename+".txt")
+		err = ioutil.WriteFile(filepath.Join(exeDir, fullpath), []byte(json.Raw), 0644) //5. 写入文件
+		if err != nil {
+			log.Fatal(err)
+		}
+		c.JSON(http.StatusOK, gin.H{"url": "/" + fullpath})
+	}
 }
